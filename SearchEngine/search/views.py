a8fafdb02dbeaf936fa1b3056a3613caa073524d
Arya <bartararya@gmail.com>
2019-12-15 20:33:34 +0330

    Add search app

--- SearchEngine/search/views.py
+++ SearchEngine/search/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+
+def search(request):
+
+
+    return HttpResponse()
