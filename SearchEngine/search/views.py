bf3b635eb64d0614757ad25d6131dc2da8aeff30
Arya <bartararya@gmail.com>
2019-12-15 20:46:27 +0330

    Moved data path

--- SearchEngine/search/views.py
+++ SearchEngine/search/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
 from django.template import loader
 
+from django.conf import settings
+
 
 def search(request):
-
-
+    print("hi")
     return HttpResponse()
